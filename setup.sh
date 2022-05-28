dbf396adf4bef165b29944215698a610911218ef
bean1310 <bean@beanlog.xyz>
2022-05-28 10:54:22 +0900

    sc

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+DATE=`date -I date`
+
+ZSHRC="${HOME}/.zshrc"
+
+if [[ -f $ZSHRC ]]; then
+    echo "Detected .zshrc"
+    echo "Rename .zshrc to .zshrc.${DATE}.bak"
+    mv $ZSHRC "${ZSHRC}.$DATE.bak"
+fi
+
+ln -s "$PWD/.zshrc" $ZSHRC
+
+TMUX_CONF="${HOME}/.tmux.conf"
+
+if [[ -f $TMUX_CONF ]]; then
+    echo "Detected .tmux.conf"
+    echo "Rename .tmux.conf to .tmux.conf.${DATE}.bak"
+    mv $TMUX_CONF "${TMUX_CONF}.$DATE.bak"
+fi
+
+ln -s "$PWD/.tmux.conf" $TMUX_CONF
